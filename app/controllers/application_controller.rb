60c9e762ea4dcfc837b36ce1ccf324556dfe8e10
Eugen Rochko <eugen@zeonfederated.com>
2013-03-09 12:19:55 +0000

    Adding German localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_locale
+
   private
+
+  def set_locale
+    I18n.locale = params[:lang] || I18n.default_locale
+  end
 
   def current_user
   	@current_user ||= session[:user_id] ? User.find(session[:user_id]) : User.new_guest(request.remote_ip)
