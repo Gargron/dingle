ca82333345f660f2f7373f3a28e033b82475ecbf
Eugen Rochko <eugen@zeonfederated.com>
2013-03-12 21:40:32 +0000

    Adding in-place edits for suggestions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,11 @@ class ReviewsController < ApplicationController
   
   def update
     @review = Review.find(params[:id])
+
+    if current_user.id != @review.user_id
+      redirect_to root_path and return
+    end
+    
     @review.update_attributes(params[:review])
     respond_with @review
   end
--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,4 +1,6 @@
 class SuggestionsController < ApplicationController
+  respond_to :html, :json
+
   def new
     @suggestion = ProviderSuggestion.new(country: Country.find(params[:country_id]))
   end
@@ -15,5 +17,15 @@ class SuggestionsController < ApplicationController
     else
       redirect_to country
     end
+  end
+
+  def update
+    if not current_user.admin?
+      redirect_to root_path and return
+    end
+
+    @suggestion = ProviderSuggestion.find(params[:id])
+    @suggestion.update_attributes(params[:suggestion])
+    respond_with @suggestion
   end
 end
